ec79810253b85659c52bcb3aef1c5deef785bad8
aliasim786 <u2267879@unimail.hud.ac.uk>
2024-10-03 14:04:50 +0100

    message

--- basic-MVC-CRUD-main/edit.php
+++ basic-MVC-CRUD-main/edit.php
@@ -1,4 +1,10 @@
 <?php
 //This file is the edit controller
-
+require('models/film.php');
 //Add some code in here to get this to work
+$id = $_GET['id'];
+$film = find($id);
+
+require('views/edit.view.php');
+
+?>
